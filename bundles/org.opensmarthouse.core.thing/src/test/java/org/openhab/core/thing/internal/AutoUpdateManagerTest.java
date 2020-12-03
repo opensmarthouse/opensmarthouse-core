e759d152dea851bb339bba5efc74550ae97e0e93
silamon <32477463+silamon@users.noreply.github.com>
2020-12-03 17:20:59 +0100

    Disable AutoUpdateManager for GroupItems (#1862)
    * Disable AutoUpdateManager for GroupItems
    
    Fixes #1330
    
    Signed-off-by: Simon Lamon <simonlamon93@hotmail.com>
    Signed-off-by: Łukasz Dywicki <luke@code-house.org>
    X-Backport-Id: 34a3487e3e29c60adcce8074ff9273e0d51a91d7

--- bundles/org.opensmarthouse.core.thing/src/test/java/org/openhab/core/thing/internal/AutoUpdateManagerTest.java
+++ bundles/org.opensmarthouse.core.thing/src/test/java/org/openhab/core/thing/internal/AutoUpdateManagerTest.java
@@ -32,6 +32,7 @@ import org.mockito.Mock;
 import org.openhab.core.events.Event;
 import org.openhab.core.events.EventPublisher;
 import org.openhab.core.items.GenericItem;
+import org.openhab.core.items.GroupItem;
 import org.openhab.core.items.MetadataRegistry;
 import org.openhab.core.items.events.ItemCommandEvent;
 import org.openhab.core.items.events.ItemEventFactory;
@@ -69,6 +70,8 @@ public class AutoUpdateManagerTest {
     private static final ChannelUID CHANNEL_UID_HANDLER_MISSING = new ChannelUID(THING_UID_HANDLER_MISSING, "channel1");
     private ItemCommandEvent event;
     private GenericItem item;
+    private ItemCommandEvent groupEvent;
+    private GroupItem groupItem;
 
     private @Mock ChannelTypeRegistry channelTypeRegistryMock;
     private @Mock EventPublisher eventPublisherMock;
@@ -90,6 +93,9 @@ public class AutoUpdateManagerTest {
         event = ItemEventFactory.createCommandEvent(ITEM_NAME, new StringType("AFTER"));
         item = new StringItem(ITEM_NAME);
         item.setState(new StringType("BEFORE"));
+        groupEvent = ItemEventFactory.createCommandEvent("groupTest", new StringType("AFTER"));
+        groupItem = new GroupItem("groupTest", new StringItem("test"));
+        groupItem.setState(new StringType("BEFORE"));
 
         when(iclRegistryMock.getLinks(eq(ITEM_NAME))).then(answer -> links);
 
@@ -346,5 +352,14 @@ public class AutoUpdateManagerTest {
 
         assertPredictionEvent("AFTER", null);
         assertStateEvent("AFTER", AutoUpdateManager.EVENT_SOURCE_OPTIMISTIC); // no?
+    }
+
+    @Test
+    public void testAutoUpdateDisabledForGroupItems() {
+        groupItem.addMember(item);
+        aum.receiveCommand(groupEvent, groupItem);
+        groupItem.removeMember(item);
+
+        assertNothingHappened();
     }
 }
