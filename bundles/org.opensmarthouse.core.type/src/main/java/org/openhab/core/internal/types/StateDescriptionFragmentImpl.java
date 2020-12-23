fde339a43e64429b9a97095d96f1cb4ee9461e1b
Wouter Born <github@maindrain.net>
2020-12-23 21:43:38 +0100

    Hide StateDescription constructor (#1995)	ce95ed191	Wouter Born <github@maindrain.net>	24 Dec 2020 at 09:43

--- bundles/org.opensmarthouse.core.type/src/main/java/org/openhab/core/internal/types/StateDescriptionFragmentImpl.java
+++ bundles/org.opensmarthouse.core.type/src/main/java/org/openhab/core/internal/types/StateDescriptionFragmentImpl.java
@@ -31,6 +31,13 @@ import org.openhab.core.types.StateOption;
 @NonNullByDefault
 public class StateDescriptionFragmentImpl implements StateDescriptionFragment {
 
+    private class StateDescriptionImpl extends StateDescription {
+        StateDescriptionImpl(@Nullable BigDecimal minimum, @Nullable BigDecimal maximum, @Nullable BigDecimal step,
+                @Nullable String pattern, boolean readOnly, @Nullable List<StateOption> options) {
+            super(minimum, maximum, step, pattern, readOnly, options);
+        }
+    }
+
     private @Nullable BigDecimal minimum;
     private @Nullable BigDecimal maximum;
     private @Nullable BigDecimal step;
@@ -153,7 +160,6 @@ public class StateDescriptionFragmentImpl implements StateDescriptionFragment {
         this.options = options;
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     public @Nullable StateDescription toStateDescription() {
         if (minimum == null && maximum == null && step == null && readOnly == null && pattern == null
@@ -161,7 +167,8 @@ public class StateDescriptionFragmentImpl implements StateDescriptionFragment {
             return null;
         }
         final Boolean ro = readOnly;
-        return new StateDescription(minimum, maximum, step, pattern, ro == null ? false : ro.booleanValue(), options);
+        return new StateDescriptionImpl(minimum, maximum, step, pattern, ro == null ? false : ro.booleanValue(),
+                options);
     }
 
     /**
