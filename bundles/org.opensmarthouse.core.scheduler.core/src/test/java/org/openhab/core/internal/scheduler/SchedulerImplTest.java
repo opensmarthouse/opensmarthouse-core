703dc6125c37cd08b6690757cd284e1e07edb27a
Wouter Born <github@maindrain.net>
2021-05-11 19:54:33 +0200

    Subject: [PATCH] Fix unstable CronSchedulerImplTest (#2354)
    This test would often fail when the system is under load.
    
    Fixes #1242
    
    Signed-off-by: Wouter Born <github@maindrain.net>
    ---
    X-Backport-Id: 0e9cecc29bfef0b71a103155581c925edb7521ed
    Signed-off-by: Chris Jackson <chris@cd-jackson.com>

--- bundles/org.opensmarthouse.core.scheduler.core/src/test/java/org/openhab/core/internal/scheduler/SchedulerImplTest.java
+++ bundles/org.opensmarthouse.core.scheduler.core/src/test/java/org/openhab/core/internal/scheduler/SchedulerImplTest.java
@@ -13,309 +13,89 @@
  */
 package org.openhab.core.internal.scheduler;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.FileNotFoundException;
-import java.lang.reflect.InvocationTargetException;
 import java.time.Duration;
 import java.time.Instant;
-import java.time.ZonedDateTime;
-import java.time.chrono.ChronoZonedDateTime;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.Map;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.openhab.core.scheduler.CronJob;
 import org.openhab.core.scheduler.ScheduledCompletableFuture;
-import org.openhab.core.scheduler.SchedulerRunnable;
-import org.openhab.core.scheduler.SchedulerTemporalAdjuster;
 
 /**
- * Test class for {@link SchedulerImpl}.
+ * Test class for {@link CronSchedulerImpl}.
  * Because the test run on scheduler all tests are guarded by a timeout to avoid having a test blocking.
  *
  * @author Peter Kriens - Initial contribution
  * @author Simon Kaufmann - adapted to Java 8
- * @author Hilbrand Bouwkamp - moved cron and periodic scheduling to it's their own interfaces
+ * @author Hilbrand Bouwkamp - moved cron scheduling to it's own class
  */
-public class SchedulerImplTest {
-    private SchedulerImpl scheduler = new SchedulerImpl();
+public class CronSchedulerImplTest {
+    private final CronSchedulerImpl cronScheduler = new CronSchedulerImpl(new SchedulerImpl());
 
-    @Test(expected = CancellationException.class, timeout = 500)
-    public void testAfterCancelled() throws InterruptedException, InvocationTargetException, ExecutionException {
-        AtomicBoolean check = new AtomicBoolean();
-        Callable<Boolean> callable = () -> check.getAndSet(true);
-        ScheduledCompletableFuture<Boolean> after = scheduler.after(callable, Duration.ofMillis(200_0000));
-        after.cancel(true);
-        Thread.sleep(100);
-        assertTrue("Scheduled job cancelled before timeout", after.isCancelled());
-        Thread.sleep(200);
-        assertFalse("Callable method should not been called", check.get());
-        after.get();
-    }
-
-    @Test(timeout = 300)
-    public void testAfterResolved() throws InterruptedException, ExecutionException {
-        AtomicInteger check = new AtomicInteger();
-        Callable<Integer> callable = () -> {
-            check.set(10);
-            return 5;
-        };
-        ScheduledCompletableFuture<Integer> after = scheduler.after(callable, Duration.ofMillis(100));
-        Thread.sleep(200);
-        assertTrue("Scheduled job should finish done", after.isDone());
-        assertEquals("After CompletableFuture should return correct value", 10, check.get());
-        assertEquals("After CompletableFuture should return correct value", 5, after.get().intValue());
-    }
-
-    @Test(expected = FileNotFoundException.class, timeout = 300)
-    public void testAfterResolvedWithException() throws Throwable {
-        Callable<Void> callable = () -> {
-            // Pass a exception not very likely thrown by the scheduler it self to avoid missing real exceptions.
-            throw new FileNotFoundException("testBeforeTimeoutException");
-        };
-        ScheduledCompletableFuture<Void> after = scheduler.after(callable, Duration.ofMillis(100));
-        Thread.sleep(200);
-        assertTrue("Scheduled job should be done", after.isDone());
-        assertTrue("After CompletableFuture should have completed with an exception",
-                after.getPromise().isCompletedExceptionally());
-        try {
-            after.get();
-        } catch (ExecutionException e) {
-            throw e.getCause();
-        }
-    }
-
-    @Test(expected = TimeoutException.class, timeout = 300)
-    public void testBeforeTimeoutException() throws Throwable {
-        CompletableFuture<Integer> d = new CompletableFuture<>();
-        ScheduledCompletableFuture<Integer> before = scheduler.before(d, Duration.ofMillis(100));
-        Thread.sleep(200);
-        d.complete(3);
-        d.get();
-        assertTrue("Scheduled job should be done", before.isDone());
-        assertTrue("Before CompletableFuture should have completed with an exception",
-                before.getPromise().isCompletedExceptionally());
-        try {
-            before.get();
-        } catch (ExecutionException e) {
-            throw e.getCause();
-        }
-    }
-
-    @Test(expected = CancellationException.class, timeout = 300)
-    public void testBeforeCancelled() throws InterruptedException, InvocationTargetException, ExecutionException {
-        CompletableFuture<Integer> d = new CompletableFuture<>();
-        ScheduledCompletableFuture<Integer> before = scheduler.before(d, Duration.ofMillis(100));
-        before.cancel(true);
-        assertTrue("Scheduled job cancelled before timeout", before.getPromise().isCompletedExceptionally());
-        before.get();
-    }
-
-    @Test(timeout = 300)
-    public void testBeforeResolved() throws InterruptedException, ExecutionException {
-        CompletableFuture<Boolean> d = new CompletableFuture<>();
-        ScheduledCompletableFuture<Boolean> before = scheduler.before(d, Duration.ofMillis(100));
-        d.complete(Boolean.TRUE);
-        assertTrue("Scheduled job should finish done", before.isDone());
-        assertTrue("Before CompletableFuture should return correct value", before.get());
-    }
-
-    @Test(expected = FileNotFoundException.class, timeout = 300)
-    public void testBeforeResolvedWithException() throws Throwable {
-        CompletableFuture<Integer> d = new CompletableFuture<>();
-        ScheduledCompletableFuture<Integer> before = scheduler.before(d, Duration.ofMillis(100));
-        // Pass an exception not very likely thrown by the scheduler it self to avoid missing real exceptions.
-        d.completeExceptionally(new FileNotFoundException("testBeforeTimeoutException"));
-        assertTrue("Scheduled job should be done", before.isDone());
-        assertTrue("Before CompletableFuture should have completed with an exception",
-                before.getPromise().isCompletedExceptionally());
-        try {
-            before.get();
-        } catch (ExecutionException e) {
-            throw e.getCause();
-        }
-    }
-
-    @Test(expected = TimeoutException.class, timeout = 300)
-    public void testAfterTimeoutException() throws Throwable {
-        CompletableFuture<Integer> d = new CompletableFuture<>();
-        ScheduledCompletableFuture<Integer> before = scheduler.before(d, Duration.ofMillis(100));
-        Thread.sleep(200);
-        d.complete(3);
-        d.get();
-        assertTrue("Scheduled job should be done", before.isDone());
-        assertTrue("Before CompletableFuture should have completed with an exception",
-                before.getPromise().isCompletedExceptionally());
-        try {
-            before.get();
-        } catch (ExecutionException e) {
-            throw e.getCause();
-        }
-    }
-
-    @Test(timeout = 1000)
-    public void testSchedule() throws InterruptedException {
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    public void testCronReboot() throws InterruptedException {
+        Instant start = Instant.now();
         Semaphore s = new Semaphore(0);
-        TestSchedulerWithCounter temporalAdjuster = new TestSchedulerWithCounter();
-        scheduler.schedule(s::release, temporalAdjuster);
-        s.acquire(3);
-        Thread.sleep(300); // wait a little longer to see if not more are scheduled.
-        assertEquals("Scheduler should not have released more after done", 0, s.availablePermits());
-        assertEquals("Scheduler should have run 3 times", 3, temporalAdjuster.getCount());
-    }
-
-    @Test(timeout = 1000)
-    public void testScheduleCancel() throws InterruptedException {
-        Semaphore s = new Semaphore(0);
-        TestSchedulerWithCounter temporalAdjuster = new TestSchedulerWithCounter();
-        ScheduledCompletableFuture<Void> schedule = scheduler.schedule(s::release, temporalAdjuster);
+        ScheduledCompletableFuture<Void> future = cronScheduler.schedule(() -> {
+        }, "@reboot");
+        future.getPromise().thenAccept(x -> s.release());
         s.acquire(1);
-        Thread.sleep(50);
-        schedule.cancel(true);
-        Thread.sleep(300); // wait a little longer to see if not more are scheduled.
-        assertEquals("Scheduler should not have released more after cancel", 0, s.availablePermits());
-        assertEquals("Scheduler should have run 1 time", 1, temporalAdjuster.getCount());
+
+        Duration duration = Duration.between(start, Instant.now());
+        Duration maxDuration = Duration.ofSeconds(2);
+        assertTrue(duration.compareTo(maxDuration) < 0,
+                "Reboot call should occur within " + maxDuration + " but was called after: " + duration);
+        assertTrue(future.isDone(), "Scheduler should be done once reboot call done.");
     }
 
-    @Test(timeout = 1000)
-    public void testScheduleException() throws InterruptedException {
-        Semaphore s = new Semaphore(0);
-        TestSchedulerWithCounter temporalAdjuster = new TestSchedulerWithCounter();
-        SchedulerRunnable runnable = () -> {
-            // Pass a exception not very likely thrown by the scheduler it self to avoid missing real exceptions.
-            throw new FileNotFoundException("testBeforeTimeoutException");
-        };
-
-        ScheduledCompletableFuture<Void> schedule = scheduler.schedule(runnable, temporalAdjuster);
-        schedule.getPromise().exceptionally(e -> {
-            if (e instanceof FileNotFoundException) {
-                s.release();
-            }
-            return null;
-        });
-        s.acquire(1);
-        Thread.sleep(300); // wait a little longer to see if not more are scheduled.
-        assertEquals("Scheduler should not have released more after cancel", 0, s.availablePermits());
-        assertEquals("Scheduler should have run 0 time", 0, temporalAdjuster.getCount());
-    }
-
-    @Test(timeout = 300)
-    public void testNegative() throws InterruptedException {
-        Semaphore s = new Semaphore(0);
-        scheduler.after(() -> {
-            s.release(1);
-            return null;
-        }, Duration.ofMillis(-1000));
-        Thread.sleep(200);
-        assertEquals("Negative value should mean after finished right away", 1, s.availablePermits());
-    }
-
-    @Test(timeout = 1000)
-    public void testDelay() throws InterruptedException {
-        long duration = 5000;
-        ScheduledCompletableFuture<Instant> future = scheduler.after(Duration.ofMillis(duration));
-        Thread.sleep(500);
-        long timeLeft = future.getDelay(TimeUnit.MILLISECONDS);
-        long expectedTimeLeft = duration - 450; // add some slack so don't check at exact time
-        assertTrue("Delay should be less:" + timeLeft + " < " + expectedTimeLeft, timeLeft < expectedTimeLeft);
-        future.cancel(true);
-    }
-
-    @Test(timeout = 1000)
-    public void testCompareTo() throws InterruptedException {
-        long duration = 5000;
-        ScheduledCompletableFuture<Instant> future1 = scheduler.after(Duration.ofMillis(duration));
-        Thread.sleep(500);
-        ScheduledCompletableFuture<Instant> future2 = scheduler.after(Duration.ofMillis(duration));
-        int compare = future1.compareTo(future2);
-        assertTrue("First future should be less than second", compare < 0);
-        future1.cancel(true);
-        future2.cancel(true);
-    }
-
-    /**
-     * This tests if the reschedule works correctly.
-     * It does this by manipulating the duration calculation of the next step.
-     * This causes the scheduler to reschedule the next call to early.
-     * It then should match against the actual expected time and reschedule because the expected time is not reached.
-     */
     @Test
     @Timeout(value = 15, unit = TimeUnit.SECONDS)
-    public void testEarlyTrigger() throws InterruptedException, ExecutionException {
-        final TestSchedulerTemporalAdjuster temporalAdjuster = new TestSchedulerTemporalAdjuster(3000);
-        final AtomicInteger counter = new AtomicInteger();
-        final SchedulerImpl scheduler = new SchedulerImpl() {
-            @Override
-            protected long currentTimeMillis() {
-                // Add 3 seconds to let the duration calculation be too short.
-                // This modification does mean it knows a bit about the internal implementation.
-                return super.currentTimeMillis() + 3000;
-            }
-        };
-        final AtomicReference<ScheduledCompletableFuture<Object>> reference = new AtomicReference<>();
-        final ScheduledCompletableFuture<Object> future = scheduler.schedule(() -> counter.incrementAndGet(),
-                temporalAdjuster);
-        reference.set(future);
-        future.get();
-        assertEquals(3, temporalAdjuster.getCount(), "The next schedule caluclator should have been done 3 times.");
-        assertEquals(3, counter.get(), "The schedule run method should have been called 3 times.");
+    public void testCronScheduling() throws InterruptedException {
+        Instant start = Instant.now();
+        AtomicReference<Object> ref = new AtomicReference<>();
+
+        Semaphore s = new Semaphore(0);
+        cronScheduler.schedule(foo -> {
+            s.release();
+            ref.set(foo.get("foo"));
+        }, Map.of("foo", "bar"), "#\n" //
+                + "\n" //
+                + " foo = bar \n" //
+                + "# bla bla foo=foo\n" //
+                + "*/5 * * * * *");
+        s.acquire(2);
+
+        Duration duration = Duration.between(start, Instant.now());
+
+        // The call should occur every 5 seconds.
+        // So the fastest execution of 2 calls would be immediately and after 5 seconds.
+        Duration minDuration = Duration.ofSeconds(5);
+
+        // The slowest execution of 2 calls would be after 2*5 seconds.
+        // When the load is high, it can be a bit slower, so we account for this by adding another 2 seconds.
+        Duration maxDuration = Duration.ofSeconds(12);
+
+        assertTrue(minDuration.compareTo(duration) < 0 && duration.compareTo(maxDuration) < 0,
+                "The two calls should be executed between " + minDuration + " and " + maxDuration
+                        + " but the total duration was: " + duration);
+        assertEquals("bar", ref.get(), "Environment variable 'foo' should be correctly set");
     }
 
-    private static class TestSchedulerWithCounter implements SchedulerTemporalAdjuster {
-        private final AtomicInteger counter = new AtomicInteger();
-
-        @Override
-        public Temporal adjustInto(Temporal arg0) {
-            return arg0.plus(100, ChronoUnit.MILLIS);
-        }
-
-        @Override
-        public boolean isDone(Temporal temporal) {
-            counter.incrementAndGet();
-            return counter.get() >= 3;
-        }
-
-        public int getCount() {
-            return counter.get();
-        }
-    }
-
-    private static class TestSchedulerTemporalAdjuster extends TestSchedulerWithCounter {
-        private final ZonedDateTime startTime;
-        private final int duration;
-
-        public TestSchedulerTemporalAdjuster(int duration) {
-            this.duration = duration;
-            startTime = ZonedDateTime.now();
-        }
-
-        @Override
-        public Temporal adjustInto(Temporal arg0) {
-            Temporal now = arg0.plus(100, ChronoUnit.MILLIS);
-            for (int i = 0; i < 5; i++) {
-                ZonedDateTime newTime = startTime.plus(duration * i, ChronoUnit.MILLIS);
-
-                if (newTime.isAfter((ChronoZonedDateTime<?>) now)) {
-                    return newTime;
-                }
-
-            }
-            throw new IllegalStateException("Test should always find time");
-        }
-
-        @Override
-        public boolean isDone(Temporal temporal) {
-            super.isDone(temporal);
-            return ZonedDateTime.now().compareTo(startTime.plus(duration * 3 - 2000, ChronoUnit.MILLIS)) > 0;
-        }
+    @Test
+    @Timeout(value = 2, unit = TimeUnit.SECONDS)
+    public void testAddRemoveScheduler() throws InterruptedException {
+        Semaphore s = new Semaphore(0);
+        CronJob cronJob = m -> s.release();
+        Map<String, Object> map = Map.of(CronJob.CRON, "* * * * * *");
+        cronScheduler.addSchedule(cronJob, map);
+        s.acquire();
+        cronScheduler.removeSchedule(cronJob);
     }
 }
